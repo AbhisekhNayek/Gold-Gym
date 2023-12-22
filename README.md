9cf6d5bf01ae4c47ec32f6fe34b27b0094b7d868
AbhisekhNayek <115914826+AbhisekhNayek@users.noreply.github.com>
2023-12-22 21:23:11 +0530

    Gold Gym Finished

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Fitness Application
+
+![React Fitness Application](https://i.ibb.co/Yt9spGc/image.png)
---
+++ src/components/Exercises.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+import Pagination from '@mui/material/Pagination';
+import { Box, Stack, Typography } from '@mui/material';
+
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+import ExerciseCard from './ExerciseCard';
+import Loader from './Loader';
+
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [exercisesPerPage] = useState(6);
+  const [errorState, setErrorState] = useState(null);
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      try {
+        let exercisesData = [];
+
+        if (bodyPart === 'all') {
+          exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+        } else {
+          exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
+        }
+
+        setExercises(exercisesData);
+      } catch (error) {
+        // Handle the error by setting the error state
+        setErrorState(error);
+      }
+    };
+
+    fetchExercisesData();
+  }, [bodyPart, setExercises]);
+
+  if (errorState) {
+    // If there is an error, render an error message
+    return <div>Error loading exercises data: {errorState.message}</div>;
+  }
+
+  if (!Array.isArray(exercises)) {
+    // If exercises is not an array, render a loader or an error message
+    return <Loader />;
+  }
+
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
+
+  const paginate = (_, value) => {
+    setCurrentPage(value);
+  };
+
+  if (!currentExercises.length) {
+    return <div>No exercises to display for the current page</div>;
+  }
+
+  return (
+    <Box id="exercises" sx={{ mt: { lg: '109px' } }} mt="50px" p="20px">
+      <Typography variant="h4" fontWeight="bold" sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="46px">Showing Results</Typography>
+      <Stack direction="row" sx={{ gap: { lg: '107px', xs: '50px' } }} flexWrap="wrap" justifyContent="center">
+        {currentExercises.map((exercise, idx) => (
+          <ExerciseCard key={idx} exercise={exercise} />
+        ))}
+      </Stack>
+      <Stack sx={{ mt: { lg: '114px', xs: '70px' } }} alignItems="center">
+        {exercises.length > 9 && (
+          <Pagination
+            color="standard"
+            shape="rounded"
+            defaultPage={1}
+            count={Math.ceil(exercises.length / exercisesPerPage)}
+            page={currentPage}
+            onChange={paginate}
+            size="large"
+          />
+        )}
+      </Stack>
+    </Box>
+  );
+};
+
+export default Exercises;
